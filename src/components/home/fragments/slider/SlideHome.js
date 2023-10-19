16a9e6db3b6f586ce435d22d3091ea24e0966b4a
Fabricio <fabriciogodoy95hotmail.com>
2023-10-19 17:54:51 -0300

    iniciando proyecto chillMama / NSX

---
+++ src/components/home/fragments/slider/SlideHome.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
+import { Link } from "react-router-dom";
+import './SliderHome.css'
+
+function SlideHome() {
+	return (
+		<Carousel className="carousel-cont">
+			<Carousel.Item className="img-carousel-item">
+				<img className="d-block w-100 img-carousel" src={`${process.env.PUBLIC_URL}/img/dibujos/dibujoDiseno2.jpg`} alt="Second slide" />
+				<Carousel.Caption className="item-carousel-caption">
+					<Link to={`/collection`} className="boton-slider">REMERAS</Link>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item className="img-carousel-item">
+				<img className="d-block w-100 img-carousel" src={`${process.env.PUBLIC_URL}/img/dibujos/dibujoDiseno.jpg`} alt="Second slide" />
+				<Carousel.Caption className="item-carousel-caption">
+					<Link to={`/collection`} className="boton-slider">BUZOS</Link>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item className="img-carousel-item">
+				<img className="d-block w-100 img-carousel" src={`${process.env.PUBLIC_URL}/img/dibujos/dibujoDiseno3.jpg`} alt="Second slide" />
+				<Carousel.Caption className="item-carousel-caption">
+					<Link to={`/collection`} className="boton-slider bg-secondary">DISEÑOS</Link>
+				</Carousel.Caption>
+			</Carousel.Item>
+		</Carousel>
+	);
+}
+
+export default SlideHome;
